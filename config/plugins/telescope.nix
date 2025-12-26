ab8b5d5c6ab3c8077c4e427ce1ad601702bb2b06
UndefinedBehaviourProphet <206461177+UndefinedBehaviorProphet@users.noreply.github.com>
2025-12-26 03:07:27 +0100

    Add telescope plugin
    Can now use telescope to search file, grep content or search tags.

---
+++ config/plugins/telescope.nix
@@ -0,0 +1,14 @@
+{
+  plugins.telescope = {
+    enable = true;
+
+    extensions.fzf-native.enable = true;
+
+    keymaps = {
+      "<leader>ff" = { action = "find_files"; options = { desc = "Telescope find file"; }; };
+      "<leader>fg" = { action = "live_grep"; options = { desc = "Telescope live grep"; }; };
+      "<leader>fb" = { action = "buffers"; options = { desc = "Telescope buffers"; }; };
+      "<leader>fh" = { action = "help_tags"; options = { desc = "Telescope help tags"; }; };
+    };
+  };
+}
