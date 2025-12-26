6efbc2a271b3b102794d71dfa28561377cbc11c1
UndefinedBehaviourProphet <206461177+UndefinedBehaviorProphet@users.noreply.github.com>
2025-12-26 08:03:18 +0100

    Enable git blame through gitsigns

--- config/plugins/gitsigns.nix
+++ config/plugins/gitsigns.nix
@@ -1,5 +1,17 @@
 {
   plugins.gitsigns = {
     enable = true;
+
+    settings = {
+      current_line_blame = true;
+      current_line_blame_opts = {
+        virt_text = true;
+        virt_text_pos = "eol";
+        delay = 1000;
+        ignore_whitespace = false;
+        virt_text_priority = 100;
+        use_focus = true;
+      };
+    };
   };
 }
