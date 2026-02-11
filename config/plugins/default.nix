fbfe05ae4e5e60e7983b17a9a797b89109d7f25e
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-02-11 18:04:47 +0100

    disable blink support

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -11,15 +11,15 @@
     ./snacks.nix
   ];
 
-  # plugins.lazy.enable = true;
+  #plugins.lazy.enable = true;
   plugins.tmux-navigator.enable = true;
-  # plugins.blame.enable = true;
   
   plugins.lsp = {
     enable = true;
     servers = {
       # Nix
       nixd.enable = true;
+      nil_ls.enable = true;
 
       # Web
       ts_ls.enable = true;
@@ -40,20 +40,23 @@
       yamlls.enable = true;
       bashls.enable = true;
       dockerls.enable = true;
-    };
-  };
 
-  plugins.blink-cmp  = {
-    enable = true;
-    settings = {
-      keymap = { preset = "default"; };
-      appearance = {
-        use_nvim_cmp_as_default = true;
-        nerd_font_variant = "mono";
-      };
-      signature = { enabled = true; }; 
+      qmlls.enable = true; # QT/qml
     };
   };
+  
+  # TODO FIX blink showing up behind or infornt of cmp also add sources to blink
+  # plugins.blink-cmp  = {
+  #   enable = true;
+  #   settings = {
+  #     keymap = { preset = "default"; };
+  #     appearance = {
+  #       use_nvim_cmp_as_default = true;
+  #       nerd_font_variant = "mono";
+  #     };
+  #     signature = { enabled = true; }; 
+  #   };
+  # };
 
   plugins.treesitter = {
     enable = true;
@@ -66,9 +69,44 @@
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
+    settings = {
+      mapping = {
+        __raw = ''
+          cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          })'';
+        };
+
+      snippet.expand = { 
+        __raw = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
+      };
+
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "nvim_lsp_signature_help"; }
+      ];
+    };
   };
-  plugins.cmp-treesitter.enable = true;
+
+  plugins.cmp-nvim-lsp-signature-help = {
+    enable = true;
+  };
+
   plugins.cmp-nvim-lsp.enable = true;
   plugins.cmp-buffer.enable = true;
   plugins.cmp-path.enable = true;
+  plugins.cmp-nvim-lua.enable = true;
+  plugins.cmp_luasnip.enable = true;
+  plugins.cmp-treesitter.enable = true;
 }
