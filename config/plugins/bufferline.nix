6df0bb11efa20036173f1d8b0a543949381def23
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-02-11 18:04:09 +0100

    Disable diagnostics indicator for bufferline

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -8,6 +8,15 @@
         right_mouse_command = { __raw = ''function(n) Snacks.bufdelete(n) end''; };
         diagnostics = "nvim_lsp";
         always_show_bufferline = true;
+        # diagnostics_indicator = { __raw =''
+        #   function(_, _, diag)
+        #     local icons = LazyVim.config.icons.diagnostics
+        #     local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+        #       .. (diag.warning and icons.Warn .. diag.warning or "")
+        #     return vim.trim(ret)
+        #   end,
+        # ''; };
+
         offsets = [
           {
             filetype = "neo-tree";
